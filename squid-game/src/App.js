0b3a3d63ea82a0b63737a6fe550ae620059d2428
IvailoManolov <ivomoreras@abv.bg>
2022-06-11 13:38:39 +0300

    added the components in the app for rendering

--- squid-game/src/App.js
+++ squid-game/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import Details from './components/Details'
+import Episodes from './components/Episodes'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Info from './components/Info'
+import Videos from './components/Videos'
 
 function App() {
   return (
-    <div>hello world!</div>
+    <div>
+      <Home/>
+      <Info/>
+      <Episodes/>
+      <Videos/>
+      <Details/>
+      <Footer/>
+    </div>
   )
 }
 
